27c9032dc251bd41702fd681ec815df97c2cb357
adityarj-github <rj.aditya2014@gmail.com>
2025-07-11 22:28:16 +0530

    Initial project commit

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,6 @@
 import logo from './logo.png'
 import logo2 from './logo2.png'
 import MyDigitalEra from './MyDigitalEra.png'
-import project from './project.png'
 import whatsapp from './whatsapp.png'
 import gmail from './gmail.png'
 import instagram from './instagram.png'
@@ -14,7 +13,6 @@ import ExrnaRare from './ExrnaRare.png'
 export const assets = {
     logo,
     logo2,
-    project,
     whatsapp,
     gmail,
     instagram,
